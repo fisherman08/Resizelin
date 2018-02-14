0a01c5c2daaa3052099df4f855dab70140eff525
kaneko <kaneko@KY-mac-mini.local>
2018-02-14 11:20:21 +0900

    testをSpekに変更

--- src/test/kotlin/com/kyproj/resizelin/ImageResizerTestCase.kt
+++ src/test/kotlin/com/kyproj/resizelin/ImageResizerTestCase.kt
@@ -3,36 +3,45 @@ package com.kyproj.resizelin
 /**
  * Created on 2018/02/02.
  */
-import junit.framework.Assert.*
-import org.junit.Test
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotSame
 import java.io.File
 
+class ImageResizerSpec :Spek({
+    describe("image risezer") {
+        val testFileName = "/imageResizer/test.jpg"
 
-class ImageResizerTestCase {
-    private val testFileName = "/imageResizer/test.jpg"
+        fun testResizer(resizer :ImageResizer) {
+            assertNotSame(0, resizer.resize(100).size)
+        }
 
-    @Test
-    fun testInitializerByteArray(){
-        val fileByteArray = this.javaClass.getResource(testFileName).readBytes()
-        val resizer = ImageResizer(fileByteArray)
-        testResizer(resizer)
+        on("initialize with byte array") {
+            val fileByteArray = this.javaClass.getResource(testFileName).readBytes()
+            val resizer = ImageResizer(fileByteArray)
+            it("can initialize"){
+                testResizer(resizer)
+            }
+        }
+
+        on("initialize with file object") {
+            val file : File = File(this.javaClass.getResource(testFileName).toURI())
+            val resizer = ImageResizer(file)
+            it("can initialize"){
+                testResizer(resizer)
+            }
+        }
+
+        on("initialize with filename"){
+            val fileName = this.javaClass.getResource(testFileName).toURI().path
+            val resizer = ImageResizer(fileName)
+            it("can initialize"){
+                testResizer(resizer)
+            }
+        }
+
     }
-
-    @Test
-    fun testInitializerFileObject(){
-        val file : File = File(this.javaClass.getResource(testFileName).toURI())
-        val resizer = ImageResizer(file)
-        testResizer(resizer)
-    }
-
-    @Test
-    fun testInitializerFileName(){
-        val fileName = this.javaClass.getResource(testFileName).toURI().path
-        val resizer = ImageResizer(fileName)
-        testResizer(resizer)
-    }
-
-    private fun testResizer(resizer :ImageResizer) {
-        assertNotSame(0, resizer.resize(100).size)
-    }
-}
+})
